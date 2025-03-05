d99bc9046b0483cd5ee74cf12fe051e350923ba4
isthispalash <thisispalash@kdio.xyz>
2025-03-04 17:33:01 -0700

    cooler looking links

--- src/component/Link.tsx
+++ src/component/Link.tsx
@@ -1,4 +1,3 @@
-
 import clsx from 'clsx';
 
 interface LinkProps {
@@ -21,13 +20,18 @@ export default function Link({
       href={href}
       onClick={onClick}
       className={clsx(
-        'text-foreground',
-        'hover:underline hover:text-xl',
+        'text-foreground relative group',
+        'hover:text-xl hover:no-underline',
         'transition-all duration-300 ease-in-out',
         className
       )}
     >
       {children}
+      <span className={clsx(
+        'absolute left-0 right-0 bottom-0 h-[1px] bg-current',
+        'scale-x-0 group-hover:scale-x-[0.8] origin-center',
+        'transition-transform duration-300 ease-in-out translate-y-1'
+      )}></span>
     </a>
   );
 }
